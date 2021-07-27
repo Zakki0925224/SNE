b7c648935ca3ea9c7839c0df580716e99d9f3f51
Zakki0925224 <zakki0925224@gmail.com>
2021-07-28 00:33:02 +0900

    プロジェクト ファイルを追加します。

---
+++ SNE/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,15 @@
+﻿using Prism.Mvvm;
+using Reactive.Bindings;
+
+namespace SNE.ViewModels
+{
+    public class MainWindowViewModel : BindableBase
+    {
+        public ReactiveProperty<string> Title { get; set; } = new ReactiveProperty<string>("SimpleNotesEditor");
+
+        public MainWindowViewModel()
+        {
+
+        }
+    }
+}
